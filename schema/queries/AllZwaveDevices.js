f46d0706f947acf21babeef87e36558e35fb5d56
Martin Stradiot <martin.stradiot@makers.sk>
2019-03-22 23:38:52 +0100

    added all zwave devices api

---
+++ schema/queries/AllZwaveDevices.js
@@ -0,0 +1,20 @@
+const {
+    GraphQLID,
+    GraphQLList,
+    GraphQLNonNull } = require('graphql');
+const { ZwaveDeviceType } = require('../types/zwave');
+const sqlite = require('sqlite-module');
+
+const AllZwaveDevices = {
+    type: new GraphQLList(ZwaveDeviceType),
+    resolve(parent, args){
+        const devices = sqlite.getAllZwaveDevices();
+        return devices.map(({ node_id, module_id, ...rest }) => ({
+            nodeId: node_id,
+            moduleId: module_id,
+            ...rest
+        }));
+    }
+};
+
+module.exports = AllZwaveDevices;
