fa10c43550d718276c97c48ae2abd51980da884a
Martin Stradiot <martin.stradiot@makers.sk>
2019-03-28 15:09:37 +0100

    bugfix

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = Object.freeze({
     port: Number(process.env.GRAPHQL_SERVER_PORT),
     certificatePath: process.env.CERTIFICATE_PATH,
     privateKeyPath: process.env.PRIVATE_KEY_PATH,
-    user: process.env.USER,
-    pass: process.env.PASS
+    user: process.env.BASIC_AUTH_USER,
+    pass: process.env.BASIC_AUTH_PASS
 });
